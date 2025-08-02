83822c101d6d888188b344af8e231f169ea22a04
usman ahmed <usman.a1282@yahoo.com>
2025-08-02 13:53:54 +0100

    ros2 jazzy mods

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -4,8 +4,9 @@ from ament_index_python.packages import get_package_share_directory
 
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclarelAunchArgument 
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from Launch.substitutions import LaunchConfiguration
 
 from launch_ros.actions import Node
 
@@ -39,19 +40,35 @@ def generate_launch_description():
             remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
         )
 
-    gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
+    #gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
 
-    # Include the Gazebo launch file, provided by the gazebo_ros package
+        default_world = os.path.join(
+            get_package_share_directory(package_name),
+            'worlds',
+            'empty.world'
+        )
+    
+        world = LaunchConfiguration('world')
+
+        world_arg = DeclarelAunchArgument(
+            'world',
+            default_value = default_world,
+            description = 'world to load'
+        )
+
+
+    # Include the Gazebo launch file, provided by the ros_gz_sim package
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-                    launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
+                    get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
+                    launch_arguments={'gz_args': ['-r -v4 ', world], 'on_exit_shutdown':'true'}.items()
              )
 
-    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
-    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+    # Run the spawner node from the ros_gz_sim package. The entity name doesn't really matter if you only have a single robot.
+    spawn_entity = Node(package='ros_gz_sim', executable='create',
                         arguments=['-topic', 'robot_description',
-                                   '-entity', 'my_bot'],
+                                   '-name', 'my_bot',
+                                   '-z':'0.1'],
                         output='screen')
 
 
@@ -91,6 +108,7 @@ def generate_launch_description():
         rsp,
         joystick,
         twist_mux,
+        world_arg,
         gazebo,
         spawn_entity,
         diff_drive_spawner,
